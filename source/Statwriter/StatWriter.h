c5a4b6853df5403bb1e6c6c2ee51d4c7d0afcdad
Niels de Waal <nielsdwaal@gmail.com>
2019-09-05 12:54:14 +0200

    Started writing stat writer, added class and started work on functions

---
+++ source/Statwriter/StatWriter.h
@@ -0,0 +1,51 @@
+#ifndef STATWRITER_H
+#define STATWRITER_H
+
+#include <EventLoop.h>
+#include <UDPSocket.h>
+
+namespace StatWriter {
+
+class StatWriter : public Common::IUDPSocketHandler
+{
+public:
+	StatWriter(EventLoop::EventLoop& ev)
+		: mEventLoop(ev)
+		, mSocket(ev, this)
+	{
+		auto streamSocketLogger = spdlog::stdout_color_mt("StatWriter");
+		mLogger = spdlog::get("StatWriter");
+	}
+
+	void InfluxConnector(const std::string& addr, const uint16_t port) const noexcept
+	{
+		mSocket.Connect(addr.c_str(), port);
+	}
+
+	void SetBatchWriting(std::chrono::seconds interval) noexcept
+	{}
+
+	/**
+	 * @brief Add variable to timed batch
+	 *
+	 * A variable is added to the list of items that get send to the TSDB at the specified interval.
+	 *
+	 * TODO Figure out a way to have a stable reference to variable in order to act like a singleton.
+	 * We want to read from an external/independent class/variable but have the 
+	 * safety of removing that reference when the data is out-of-use.
+	 * IDEAS:
+	 *	- Template ref with name (e.g template<auto& var, string name>)
+	 */
+	//void AddToBatch() noexcept
+
+private:
+	EventLoop::EventLoop& mEventLoop;
+
+	UDPSocket mSocket;
+
+	std::shared_ptr<spdlog::logger> mLogger;
+};
+
+}
+
+#endif // STATWRITER_H
