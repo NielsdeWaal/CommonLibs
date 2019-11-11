37ba5d6ccc05588f40f2dd95432d0e9f61133d12
Niels <nielsdwaal@gmail.com>
2019-11-11 15:32:23 +0100

    Added hostname resolution for TLSSocket

--- source/Common/TLSSocket.h
+++ source/Common/TLSSocket.h
@@ -1,6 +1,10 @@
 #ifndef TLS_SOCKET_H
 #define TLS_SOCKET_H
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
@@ -81,6 +85,33 @@ public:
 			mEventLoop.RegisterFiledescriptor(mFd, EPOLLIN, this);
 			mLogger->info("fd:{} connected instantly", mFd);
 		}
+	}
+
+	void ConnectHostname(const std::string& url, const uint16_t port) noexcept
+	{
+		struct addrinfo hints;
+		struct addrinfo *infoptr;
+		memset(&hints, 0, sizeof hints);
+		hints.ai_family = AF_INET; // AF_INET means IPv4 only addresses
+		hints.ai_socktype = SOCK_STREAM; // Only want stream-based connection
+		hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+		const int result = ::getaddrinfo(url.c_str(), NULL, &hints, &infoptr);
+		if(result)
+		{
+			//fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result));
+			mLogger->critical("getaddrinfo: {}", gai_strerror(result));
+			throw std::runtime_error("getaddrinfo");
+		}
+
+		//struct addrinfo *p;
+		char host[256];
+		getnameinfo(infoptr->ai_addr, infoptr->ai_addrlen, host, sizeof (host), NULL, 0, NI_NUMERICHOST);
+		mLogger->info("Resolved {} to {}", url, std::string{host});
+
+		Connect(host, port);
+
+		freeaddrinfo(infoptr);
 	}
 
 	void Send(const char* data, const size_t len) noexcept
